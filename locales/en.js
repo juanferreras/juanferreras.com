e1b5f27e6db2f8414d5629afad5549d47af47673
Juan Ferreras <juan@flioh.com>
2016-02-21 09:22:52 -0300

    More translations

--- locales/en.js
+++ locales/en.js
@@ -6,7 +6,19 @@
 	"Crafting wonderful": "Crafting wonderful things at Flioh!",
 	"Entrepreneur": "Entrepreneur. Designer and Developer.",
 	"Contact": "Contact",
-	"Performance Testing": "Performance Testing",
-  "Hey Juan": "Hey Juan! My name is "
+  "Hey Juan": "Hey Juan! My name is ",
+  "in need of": "I'm in need of ",
+  "a website": "a website",
+  "brand design": "brand design",
+  "app development": "app development",
+  "motion design": "motion design",
+  "graphic design": "graphic design",
+  "something else": "something else",
+  "rough budget of": "I've got a rough budget of ",
+  "Please reach out at": "Please reach out at",
+  "The world is full": "The world is full of obvious things which nobody by any chance ever observes.",
+  "said Sherlock Holmes once": "— said Sherlock Holmes once. It's the ",
+  "time you visit this section": " time you visit this section. If you need any help, please feel free to",
+  "email me": "email me."
 }
 
--- locales/es.js
+++ locales/es.js
@@ -6,6 +6,18 @@
 	"Crafting wonderful": "Creando cosas geniales en Flioh!",
 	"Entrepreneur": "Emprendedor. Diseñador y Desarrollador.",
 	"Contact": "Contacto",
-	"Performance Testing": "Performance Testing",
-  "Hey Juan": "Hola Juan! Mi nombre es "
+  "Hey Juan": "¡Hola Juan! Mi nombre es ",
+  "in need of": "Estoy interesado en ",
+  "a website": "una página web",
+  "brand design": "identidad corporativa",
+  "app development": "desarrollo de apps",
+  "motion design": "motion design",
+  "graphic design": "diseño gráfico",
+  "something else": "otra cosa",
+  "rough budget of": "Tengo un presupuesto aproximado de ",
+  "Please reach out at": "Por favor, contáctame a",
+  "The world is full": "El mundo está lleno de cosas obvias que nadie, ni por casualidad observa jamás.",
+  "said Sherlock Holmes once": "— dijo Sherlock Holmes. Es la ",
+  "time you visit this section": " vez que visitás esta sección. Si necesitás ayuda, por favor contactame directamente ",
+  "email me": "via email."
 }
