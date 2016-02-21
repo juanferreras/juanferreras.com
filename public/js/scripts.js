02f441f25dda8b273958d5affde28fc73e5fb797
Juan Ferreras <juan@flioh.com>
2016-02-20 21:21:38 -0300

    Fixed contact breakpoints
    -

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3083,43 +3083,6 @@
 });
 
 /*
- * debouncedresize: special jQuery event that happens once after a window resize
- *
- * latest version and complete README available on Github:
- * https://github.com/louisremi/jquery-smartresize
- *
- * Copyright 2012 @louis_remi
- * Licensed under the MIT license.
- *
- * This saved you an hour of work? 
- * Send me music http://www.amazon.co.uk/wishlist/HNTU0468LQON
- */
-(function($) {
-    var $event = $.event, $special, resizeTimeout;
-    $special = $event.special.debouncedresize = {
-        setup: function() {
-            $(this).on("resize", $special.handler);
-        },
-        teardown: function() {
-            $(this).off("resize", $special.handler);
-        },
-        handler: function(event, execAsap) {
-            // Save the context
-            var context = this, args = arguments, dispatch = function() {
-                // set correct event type
-                event.type = "debouncedresize";
-                $event.dispatch.apply(context, args);
-            };
-            if (resizeTimeout) {
-                clearTimeout(resizeTimeout);
-            }
-            execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
-        },
-        threshold: 150
-    };
-})(jQuery);
-
-/*
  * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
  *
  * Uses the built in easing capabilities added In jQuery 1.1
